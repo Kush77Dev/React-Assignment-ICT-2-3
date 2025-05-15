385081eb878b63fef50b6c74d152e5221a2f9b76
Kush77Dev <unknowndevloper77@gmail.com>
2025-05-15 14:11:41 +0530

    first

---
+++ Apps/KotlinAssignment2/app/src/main/java/com/example/kotlinassignment2/Question1/Employee.kt
@@ -0,0 +1,10 @@
+data class Employee(
+    val code: String,
+    val name: String,
+    val designation: String,
+    val dob: String,
+    val dateOfJoining: String,
+    val basicSalary: Double,
+    val department: String,
+    val totalSalary: Double
+)
